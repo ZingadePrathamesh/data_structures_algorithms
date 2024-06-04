8b235e9b160ae349921a419ef5e2b8de2d9c0d43
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2024-06-04 14:57:42 +0530

    splitting the array

--- binarysearch/src/SplitTheArrayIntoM.java
+++ binarysearch/src/SplitTheArrayIntoM.java
@@ -4,6 +4,7 @@ public class SplitTheArrayIntoM {
     public static void main(String[] args) {
         int[] nums = {1,2,3,4,5};
         System.out.println(splitTheArray(nums, 2));
+        System.out.println(splitTheArray2(nums, 2));
     }
 
     public static int splitTheArray(int[] nums, int m){
@@ -41,5 +42,31 @@ public class SplitTheArrayIntoM {
             if(i > max) max = i;
         }
         return max;
+    }
+
+    public static int splitTheArray2(int[] nums, int splits){
+        int start = largestElement(nums);
+        int end = sumOfArray(nums);
+        int mid =  0;
+        int sum = 0;
+        if(splits == nums.length) return largestElement(nums);
+        if(splits == 1) return sumOfArray(nums);
+        else{
+            while(start<end){
+                int pieces = 1;
+                sum =  0;
+                mid = start + (end - start)/2;
+                for(int i : nums){
+                    if((sum+i) <= mid) sum+=i;
+                    else{
+                        sum = i;
+                        pieces++;
+                    }
+                }
+                if(pieces <= splits) end = mid;
+                else start = mid+1;
+            }
+            return start;
+        }
     }
 }
