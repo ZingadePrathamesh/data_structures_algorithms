c57e8d7cbbd3eaae3171759030eebd03762f361f
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2024-06-05 13:19:14 +0530

    binary search in 2d arrays

--- binarysearchin2d/src/BinarySearch2d.java
+++ binarysearchin2d/src/BinarySearch2d.java
@@ -9,7 +9,17 @@ public class BinarySearch2d {
                 {13, 14, 15, 16}
         };
 
-        System.out.println(Arrays.toString(binarySearchIn2d(nums, 13)));
+        int[][] nums2= {
+            {10, 20, 30, 40},
+                {12, 22, 32, 42},
+                {14, 24, 34, 44},
+        };
+        for (int i = 0; i< 17; i++){
+            System.out.println(Arrays.toString(binarySearchIn2d(nums, i)));
+        }
+
+//        System.out.println(Arrays.toString(binarySearchIn2DArrays(nums, 8, 0, nums[0].length-1)));
+//        System.out.println(Arrays.toString(binarySearchIn2DArrays(nums2, 12, 0, nums2[0].length-1)));
     }
 
 
@@ -23,34 +33,73 @@ public class BinarySearch2d {
         return new int[]{-1, -1};
     }
 
-
     public static int[] binarySearchIn2d(int[][] nums, int target){
-        int row = nums.length-1;
-        int col = nums[0].length-1;
-        int rStart = 0, rEnd = row;
-        int cStart = 0, cEnd = col;
-        int cMid = cStart + (cEnd-cStart)/2;
-        if(row == 1){
-            return binarySearchIn1D(nums, 0, 0, col, target);
+        int rowStart = 0; int rowEnd = nums.length-1;
+        int colStart = 0; int colEnd = nums[0].length-1;
+        int rowMid =  0; int colMid =0;
+        int[] result = {-1, -1};
+
+        //until we get to only two remaining rows
+        while(rowStart<rowEnd-1){
+            colMid = colStart + (colEnd - colStart)/2;
+            rowMid = rowStart + (rowEnd - rowStart)/2;
+            if(nums[rowMid][colMid] == target){
+                return new int[]{rowMid, colMid};
+            }
+            else if(nums[rowMid][colMid] > target) rowEnd--;
+            else rowStart++;
         }
 
-        while(rStart<(rEnd-1)){
-            int rMid= rStart + (rEnd-rStart)/2;
-            if(target == nums[rStart][cMid]) return new int[]{rStart, cMid};
-            if(target < nums[rStart][cMid]){
-                rEnd = rMid;
-            }
-            else {
-                rStart = rMid;
-            }
+        //now we have two remaining rows so we will perform searches in five areas
+        //middle column, top left, top right, bottom left, bottom right
+
+        //middle column
+        if(nums[rowStart][colMid] == target){
+            return new int[]{rowStart, colMid};
+        }
+        if(nums[rowEnd][colMid] == target){
+            return new int[]{rowEnd, colMid};
         }
 
-        if(target == nums[rStart][cMid]) return new int[]{rStart, cMid};
-        if(target == nums[rStart+1][cMid]) return new int[]{rStart+1, cMid};
+        //searching in other four spaces horizontally
+        result = binarySearchIn1DArray(nums, target, rowStart, 0, colMid-1);
+        if(result[0] == -1){
+            result = binarySearchIn1DArray(nums, target, rowEnd, 0, colMid-1);
+        }if(result[0] == -1){
+            result = binarySearchIn1DArray(nums, target, rowStart, colMid+1, colEnd);
+        }if(result[0] == -1){
+            result = binarySearchIn1DArray(nums, target, rowEnd, colMid+1, colEnd);
+        }
 
-        if(target<nums[rStart][cMid] && target>=nums[rStart][0]) return binarySearchIn1D(nums, rStart, 0, cMid-1, target);
-        if(target>nums[rStart][cMid] && target<=nums[rStart][col]) return binarySearchIn1D(nums, rStart, cMid+1, col, target);
-        if(target<nums[rStart+1][cMid] && target>=nums[rStart+1][0]) return binarySearchIn1D(nums, rStart+1, 0, cMid-1, target);
-        else return binarySearchIn1D(nums, rStart+1, cMid+1, col, target);
+        return result;
+     }
+
+     public static int[] binarySearchIn1DArray(int[][] nums, int target, int row, int colStart, int colEnd){
+        int colMid = 0;
+        while(colStart<=colEnd){
+            colMid = colStart + (colEnd - colStart)/2;
+            if(nums[row][colMid] == target){
+                return new int[]{row, colMid};
+            }
+            else if(nums[row][colMid] > target) colEnd = colMid-1;
+            else colStart = colMid+1;
+        }
+        return new int[]{-1, -1};
+     }
+
+    public static int[] binarySearchIn2DArrays(int[][] nums, int target, int rowStart, int colStart){
+        if(nums.length == 0 || nums== null){
+            System.out.println("Invalid Array");
+            return new int[]{-1,-1};
+        }
+        else{
+            while(rowStart <= nums.length-1 && colStart >= 0 ){
+                if(nums[rowStart][colStart] == target) return new int[]{rowStart, colStart};
+                else if(nums[rowStart][colStart] > target) colStart--;
+                else rowStart++;
+            }
+            System.out.println("Target not found");
+            return new int[]{-1,-1};
+        }
     }
 }
