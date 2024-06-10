e19c57e2bfeffd0ae0030b592cdcfb032e233188
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2024-06-10 16:09:45 +0530

    Solved few questions regarding strings

--- LearningString/src/ShuffleString.java
+++ LearningString/src/ShuffleString.java
@@ -1,5 +1,14 @@
 public class ShuffleString {
     public static void main(String[] args) {
-        
+        String str = "codeleet";
+        int[] indices ={4,5,6,7,0,2,1,3};
+        System.out.println(restoreString(str, indices));
+    }
+    public static String restoreString(String s, int[] indices) {
+        StringBuilder result = new StringBuilder(s);
+        for(int i = 0; i<indices.length; i++){
+            result.setCharAt(indices[i], s.charAt(i));
+        }
+        return result.toString();
     }
 }
