ca98b9ac2207f497a65f381bcd5304cd78f44fdd
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2024-06-11 14:48:44 +0530

    Doing pattern problems

--- PatternPrinting/src/pattern1.java
+++ PatternPrinting/src/pattern1.java
@@ -5,6 +5,9 @@ public class pattern1 {
         pattern1(5);
         pattern2(5);
         pattern3(5);
+        pattern4(5);
+        pattern5(5);
+        pattern6(5);
     }
 
     public static void pattern1(int n){
@@ -27,6 +30,42 @@ public class pattern1 {
         for(int i = 0; i<n; i++){
             for(int j = 0; j<=i;j++){
                 System.out.printf("%d ", j+1);
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern4(int n){
+        for(int i = 0; i<2*n; i++){
+            int c = i<=n?i:2*n-i;
+            for(int j = 0; j<c; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern5(int n){
+        for(int i = 0; i<n; i++){
+            int c = i<=n?i:2*n-i;
+            for(int j = 0; j<n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 0; j<c; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern6(int n){
+        int col =  0;
+        System.out.println("\n\n\nPattern 6\n");
+        for(int i = 0; i<2*n; i++){
+            col =  i<n?n-i:i-n+1;
+            int spaces = n-col;
+            for(int j = 0; j<spaces; j++){
+                System.out.print(" ");
+            }
+            for(int j = 0; j<col; j++){
+                System.out.print("* ");
             }
             System.out.println();
         }
