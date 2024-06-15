3fb1b0f6c7a86d4ab7f694841022dde0603f25a6
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2024-06-15 16:02:00 +0530

    Solved the fibonacciseries using mathematics

--- Recursion/src/FibonacciSeries.java
+++ Recursion/src/FibonacciSeries.java
@@ -1,11 +1,27 @@
 public class FibonacciSeries {
     public static void main(String[] args) {
         System.out.println(fibonacciSeries(40));
+        System.out.println(fibonacciSeriesUsingMaths(40));
+        System.out.println(fibonacciSeriesUsingMaths(0));
+        System.out.println(fibonacciSeriesUsingMaths(1));
+        System.out.println(fibonacciSeriesUsingMaths(2));
+        System.out.println(fibonacciSeriesUsingMaths(3));
+        System.out.println(fibonacciSeriesUsingMaths(4));
     }
     public static long fibonacciSeries(long n){
         if(n == 0 || n == 1) return n;
         else{
             return fibonacciSeries(n-1) + fibonacciSeries(n-2);
         }
+    }
+
+    public static long fibonacciSeriesUsingMaths(int n){
+        double result = 0;
+        double a =  Math.sqrt(5);
+        double b =  Math.pow(((1+Math.sqrt(5))/2),n);
+        double c =  Math.pow(((1-Math.sqrt(5))/2),n);
+
+        result = (b-c)/a;
+        return (long) result;
     }
 }
