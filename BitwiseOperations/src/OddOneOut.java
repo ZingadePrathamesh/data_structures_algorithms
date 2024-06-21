86e9a6854f8262fdc08cb7ae4654ca950480fe05
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2024-06-21 11:49:34 +0530

    solved the odd man out from a array of duplicated nunbers of the number using bitwise operations

--- BitwiseOperations/src/OddOneOut.java
+++ BitwiseOperations/src/OddOneOut.java
@@ -3,6 +3,7 @@ public class OddOneOut {
         int[] nums = {1, 1, 1, 2, 4, 5, 4, 2, 4, 2};
         //5 appeared only once.
         System.out.println(oddManOut(nums,3));
+        System.out.println(oddNumberOut(nums,3));
     }
 
     public static int oddManOut(int nums[], int n){
@@ -19,6 +20,21 @@ public class OddOneOut {
             if(arrayBit[i] % n != 0){
                 result |= (1<<i);
             }
+        }
+        return result;
+    }
+
+    public static int oddNumberOut(int nums[], int repetition){
+        int[] bitCount = new int[32];
+        for(int num: nums){
+            for(int i = 0; i<32; i++){
+                if((num & (1<<i)) != 0) bitCount[i]++;
+            }
+        }
+
+        int result = 0;
+        for(int i = 0; i<32; i++ ){
+            if(bitCount[i]%repetition != 0) result |= 1<<i;
         }
         return result;
     }
