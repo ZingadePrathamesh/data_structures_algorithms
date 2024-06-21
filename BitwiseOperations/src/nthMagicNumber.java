a7a61a6141149ca8e2c6e8911af7efa1ac78b21b
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2024-06-21 12:29:04 +0530

    solved the nth magic number using bitwise operations

--- BitwiseOperations/src/nthMagicNumber.java
+++ BitwiseOperations/src/nthMagicNumber.java
@@ -2,6 +2,7 @@ public class nthMagicNumber {
     public static void main(String[] args) {
         for(int i  = 1; i< 10; i++){
             System.out.println(magicNumber(i));
+            System.out.println(magicNumberByKunal(i));
         }
     }
     public static int magicNumber(int term){
@@ -10,8 +11,21 @@ public class nthMagicNumber {
         else{
             int magicNumber = 0;
             for(int i = 0; i<32; i++){
-                System.out.println(Integer.toBinaryString(1<<i));
                 if((term & (1<<i)) != 0) magicNumber += (int) Math.pow(5, i+1);
+            }
+            return magicNumber;
+        }
+    }
+    public static int magicNumberByKunal(int term){
+        int base = 5;
+        int magicNumber = 0;
+        if(term == 1) return 5;
+        else{
+            while(term>0){
+                int last =  term & 1;
+                term = term >> 1;
+                magicNumber += last * base;
+                base *= 5;
             }
             return magicNumber;
         }
