6b4b8df4c0a65c52d011813fba5c9b65551a3ec6
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2024-06-23 21:17:42 +0530

    Completed the bitwise operation video and solved many questions over leetcode.

--- BitwiseOperations/src/XORArray.java
+++ BitwiseOperations/src/XORArray.java
@@ -1,6 +1,7 @@
 public class XORArray {
     public static void main(String[] args) {
-
+        System.out.println(xorOperationsTillN(10));
+        System.out.println(xorOperationInRange(0, 10));
     }
     public static int xorOperation(int n, int start) {
         int result = 0;
@@ -8,5 +9,25 @@ public class XORArray {
             result ^= start + 2 * i;
         }
         return result;
+    }
+
+    public static int xorOperationsTillN(int  num){
+        int a = num % 4 ;
+        switch (a){
+            case 0:
+                return num;
+            case 1:
+                return 1;
+            case 2:
+                return num+1;
+            case 3:
+                return 0;
+        }
+        return a;
+    }
+
+    public static int xorOperationInRange(int start, int end){
+        if(start == 0) return xorOperationsTillN(end);
+        return xorOperationsTillN(end) ^ xorOperationsTillN(start-1);
     }
 }
