b4c86f597df60c29c15ab7265c8f5e9f07e55561
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2024-06-28 21:51:44 +0530

    solved bitwise operation problem of the leetcode.

---
+++ Mathematics/src/Mathematics.java
@@ -0,0 +1,44 @@
+public class Mathematics {
+    public static void main(String[] args) {
+        int[] combinedArray = {
+                2, 3, 5, 7, 11, 13, 17, 19, 23, 29,  // Prime numbers
+                4, 6, 8, 9, 10, 12, 14, 15, 16, 18   // Non-prime numbers
+        };
+
+        for(int num: combinedArray){
+            String number = isPrimeNumberOptimised((long) num)?"Prime":"Not Prime";
+            System.out.println(num + " : " + number);
+        }
+        System.out.println(Long.MAX_VALUE + " : " + isPrimeNumberOptimised(Long.MAX_VALUE));
+    }
+
+    /**
+     * Checks if a given number is prime using an optimized approach.
+     *
+     * @param number the number to check
+     * @return true if the number is prime, false otherwise
+     */
+    public static boolean isPrimeNumberOptimised(long number) {
+        // Numbers less than or equal to 1 are not prime
+        if (number <= 1) return false;
+
+        // Numbers 2 and 3 are prime
+        if (number <= 3) return true;
+
+        // Eliminate even numbers and multiples of 3
+        if (number % 2 == 0 || number % 3 == 0) return false;
+
+        // Check for factors from 5 onwards using the 6k ± 1 optimization
+        for (long i = 5; (i * i) <= number; i += 6) {
+            // Check if the number is divisible by i or (i + 2)
+            if (number % i == 0 || number % (i + 2) == 0) {
+                return false;
+            }
+        }
+
+        // If no factors are found, the number is prime
+        return true;
+    }
+
+
+}
