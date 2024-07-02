2ad27ac2ef2a9216e72f38caca626f3e76f23224
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2024-07-02 12:35:59 +0530

    Mathematics for DSA.

--- Mathematics/src/SquareRoot.java
+++ Mathematics/src/SquareRoot.java
@@ -1,9 +1,18 @@
 public class SquareRoot {
     public static void main(String[] args) {
-
+        System.out.printf("Square root of 40 is %.3f" ,preciseSquareRoot(40));
     }
     public static double preciseSquareRoot(int number){
-
+        double x = number;
+        double root;
+        while(true){
+            root = 0.5 * (x + (number/x));
+            if(Math.abs(x - root) < 0.1){
+                break;
+            }
+            x = root;
+        }
+        return root;
     }
 
 }
