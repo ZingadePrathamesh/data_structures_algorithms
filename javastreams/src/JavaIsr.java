50caccf95f919ca2df9f9bccaef5145c7915a26f
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2024-09-19 15:49:52 +0530

    Learning file input output

---
+++ javastreams/src/JavaIsr.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class JavaIsr {
+    public static void main(String[] args) {
+        try(OutputStream outputStream = new FileOutputStream("output.txt");){
+            outputStream.write("hello".getBytes(StandardCharsets.UTF_8));
+        }
+        catch (IOException ioException){
+            System.out.println(ioException.getMessage());
+        }
+
+        try(InputStreamReader inputStreamReader = new InputStreamReader(System.in)){
+            System.out.println("Enter some letters to print");
+            int letters = inputStreamReader.read();
+            while(inputStreamReader.ready()){
+                System.out.println((char) letters);
+                letters = inputStreamReader.read();
+            }
+        }
+        catch (IOException ioException){
+            System.out.println(ioException.getMessage());
+        }
+
+        try(FileReader fileReader = new FileReader("output.txt")){
+            while(fileReader.ready()){
+                System.out.println((char) fileReader.read());
+            }
+        }
+        catch (IOException ioException){
+            System.out.println(ioException.getMessage());
+        }
+    }
+}
